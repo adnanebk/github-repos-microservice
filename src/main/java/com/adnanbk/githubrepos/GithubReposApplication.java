6269038c5340c7a85c9d99b8b98cc975b8daa26a
benkouider.adnane@gmail.com <mssadmine01>
2020-10-27 16:45:00 -0700

    remove eureka client dependency

--- src/main/java/com/adnanbk/githubrepos/GithubReposApplication.java
+++ src/main/java/com/adnanbk/githubrepos/GithubReposApplication.java
@@ -4,7 +4,6 @@ import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
