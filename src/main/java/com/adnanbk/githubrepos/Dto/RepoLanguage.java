43fdbed4c0563d50acde6a1799c249e83b9154b3
adnanebk <31042158+adnanebk@users.noreply.github.com>
2020-10-27 12:14:13 +0000

    change set to list

--- src/main/java/com/adnanbk/githubrepos/Dto/RepoLanguage.java
+++ src/main/java/com/adnanbk/githubrepos/Dto/RepoLanguage.java
@@ -1,6 +1,8 @@
 package com.adnanbk.githubrepos.Dto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RepoLanguage {
@@ -8,42 +10,15 @@ public class RepoLanguage {
 
   private long numberOfRepos;
   private String name;
-  private Set<GithubRepo> listOfRepos=new HashSet<>();
-
-    public RepoLanguage(String name) {
-        this.name = name;
-    }
-
-    public RepoLanguage(String name, long numberOfRepos, Set<GithubRepo> listOfRepos) {
-        this.numberOfRepos = numberOfRepos;
-        this.name = name;
-        this.listOfRepos = listOfRepos;
-    }
-
-    public long getNumberOfRepos() {
-        return numberOfRepos;
-    }
-
-    public void setNumberOfRepos(long numberOfRepos) {
-        this.numberOfRepos = numberOfRepos;
-    }
-
-    public Set<GithubRepo> getListOfRepos() {
-        return listOfRepos;
-    }
-
-    public void setListOfRepos(Set<GithubRepo> listOfRepos) {
-        this.listOfRepos = listOfRepos;
-    }
+  private List<GithubRepo> listOfRepos=new ArrayList<>();
 
     public RepoLanguage() {
     }
 
-    public String getName() {
-        return name;
+    public RepoLanguage(String name, long numberOfRepos, List<GithubRepo> listOfRepos) {
+        this.numberOfRepos = numberOfRepos;
+        this.name = name;
+        this.listOfRepos = listOfRepos;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
